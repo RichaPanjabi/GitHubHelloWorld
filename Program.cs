953be7e1714b4116f559e6bb441eb28d5109c64f
RichaPanjabi <richa.panjabi@weltec.ac.nz>
2026-05-18 13:34:22 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GitHubHelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to GitHub Lecture");
+            Console.WriteLine("Learning GitHub with Visual Studio 2022");
+            Console.ReadLine();
+        }
+    }
+}
